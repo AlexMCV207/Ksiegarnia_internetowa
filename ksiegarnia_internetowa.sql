444edc19ba1c62ed9c711e69c3a8b4c5d8542113
AlexMCV207 <113392257+AlexMCV207@users.noreply.github.com>
2024-06-02 00:38:14 +0200

    Add files via upload

--- ksiegarnia_internetowa.sql
+++ ksiegarnia_internetowa.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 31 Sty 2018, 10:36
--- Wersja serwera: 10.1.19-MariaDB
--- Wersja PHP: 5.6.28
+-- Generation Time: Cze 02, 2024 at 12:37 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `ksiegarnia_internetowa2`
+-- Database: `ksiegarnia_internetowa`
 --
 
 -- --------------------------------------------------------
@@ -28,48 +29,87 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `klient` (
   `Id_klienta` int(11) NOT NULL,
-  `Nazwisko` varchar(30) COLLATE utf8_polish_ci NOT NULL,
-  `Imie` varchar(15) COLLATE utf8_polish_ci NOT NULL,
-  `Kod_pocztowy` text COLLATE utf8_polish_ci NOT NULL,
-  `Miejscowosc` varchar(20) COLLATE utf8_polish_ci NOT NULL,
-  `Ulica` varchar(15) COLLATE utf8_polish_ci NOT NULL,
-  `Nr_domu` text COLLATE utf8_polish_ci NOT NULL,
+  `Nazwisko` varchar(30) NOT NULL,
+  `Imie` varchar(15) NOT NULL,
+  `Kod_pocztowy` text NOT NULL,
+  `Miejscowosc` varchar(20) NOT NULL,
+  `Ulica` varchar(15) NOT NULL,
+  `Nr_domu` text NOT NULL,
   `PESEL` int(11) NOT NULL,
   `Telefon` int(11) NOT NULL,
-  `AdresEmail` text COLLATE utf8_polish_ci NOT NULL
+  `AdresEmail` text NOT NULL,
+  `login` varchar(30) DEFAULT NULL,
+  `haslo` varchar(30) DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Zrzut danych tabeli `klient`
+-- Dumping data for table `klient`
 --
 
-INSERT INTO `klient` (`Id_klienta`, `Nazwisko`, `Imie`, `Kod_pocztowy`, `Miejscowosc`, `Ulica`, `Nr_domu`, `PESEL`, `Telefon`, `AdresEmail`) VALUES
-(1, 'Jorek', 'Ewa', '33-100', 'Tarnów', 'Kościuszki', '15/9', 0, 451421512, 'ewa@02.pl'),
-(2, 'Kicior', 'Marek', '33-100', 'Tarnów', 'Lwowska', '12', 0, 215121215, 'marek@poczta.pl'),
-(3, '', '', '', '', '', '', 0, 0, ''),
-(4, '', '', '', '', '', '', 0, 0, ''),
-(5, 'Nosalski', 'Egon', '25-124', 'Robaki', 'Kornika', '56', 0, 542121212, 'kornik1@nowy.com'),
-(6, 'Żabka', 'Kacper', '78-512', 'Londonek', 'Korce', '325/12', 0, 741741741, 'Korce@dkdal.com');
+INSERT INTO `klient` (`Id_klienta`, `Nazwisko`, `Imie`, `Kod_pocztowy`, `Miejscowosc`, `Ulica`, `Nr_domu`, `PESEL`, `Telefon`, `AdresEmail`, `login`, `haslo`, `admin`) VALUES
+(1, 'Jorek', 'Ewa', '33-100', 'Tarnów', 'Kościuszki', '15/9', 0, 451421512, 'ewa@02.pl', NULL, NULL, 0),
+(2, 'Kicior', 'Marek', '33-100', 'Tarnów', 'Lwowska', '12', 0, 215121215, 'marek@poczta.pl', NULL, NULL, 0),
+(5, 'Nosalski', 'Egon', '25-124', 'Robaki', 'Kornika', '56', 0, 542121212, 'kornik1@nowy.com', NULL, NULL, 0),
+(6, 'Żabka', 'Kacper', '78-512', 'Londonek', 'Korce', '325/12', 0, 741741741, 'Korce@dkdal.com', NULL, NULL, 0),
+(31, 'Kowalski', 'Jan', '33-100', 'Tarnow', 'Szujskiego', '45', 0, 837927165, 'j.kowalski@w.pl', 'JK2024', '12345', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ksiazki`
+--
+
+CREATE TABLE `ksiazki` (
+  `id` int(11) NOT NULL,
+  `tytul` varchar(50) DEFAULT NULL,
+  `autor` varchar(50) DEFAULT NULL,
+  `data_wydania` date DEFAULT NULL,
+  `cena` int(11) DEFAULT NULL,
+  `ilosc` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ksiazki`
+--
+
+INSERT INTO `ksiazki` (`id`, `tytul`, `autor`, `data_wydania`, `cena`, `ilosc`) VALUES
+(0, 'Pan Tadeusz', 'Adam Mickiewicz', '1834-01-01', 40, 63),
+(1, 'Dziady cz. III', 'Adam Mickiewicz', '1832-01-01', 25, 55),
+(2, 'Kordian', 'Juliusz Slowacki', '1834-01-01', 30, 19),
+(3, 'Lalka', 'Boleslaw Prus', '1890-01-01', 53, 107),
+(4, 'Makbet', 'William Shakespeare', '1606-01-01', 30, 92),
+(5, 'Wesele', 'Stanislaw Wyspianski', '1901-03-16', 40, 60),
+(6, 'Skapiec', 'Molier', '1668-09-09', 27, 129),
+(7, 'Antygona', 'Sofokles', '0000-00-00', 20, 84);
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `klient`
+-- Indeksy dla tabeli `klient`
 --
 ALTER TABLE `klient`
   ADD PRIMARY KEY (`Id_klienta`);
+
+--
+-- Indeksy dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `klient`
+-- AUTO_INCREMENT for table `klient`
 --
 ALTER TABLE `klient`
-  MODIFY `Id_klienta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Id_klienta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
